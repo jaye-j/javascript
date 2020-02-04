6377033a0a785202e6484f4b5337472790e90959
jaye-j <jacobrobertjensen@gmail.com>
2020-02-04 11:22:20 -0600

    js practice

---
+++ class.js
@@ -0,0 +1,50 @@
+// 02/04/2020 class work //
+console.log('hello');
+
+document.body.innerHTML = 'Hello World';
+
+//
+var school = 'dig crafts'; //string
+
+console.log(school);
+
+function myFunction(){
+
+};
+
+//
+var multiLineString = `This
+is a
+multi line
+string`;
+
+console.log(multiLineString);
+console.log(multiLineString.length);
+
+var myInt = 2.1; //float
+
+var myBool = true; //boolean
+
+var num = 1; //int
+
+var num2 = 1.2; //float
+
+console.log(school + ' ' + multiLineString); //concatenation
+
+var concat = "hello" + ' ' + "world";
+console.log(concat.indexOf("world"));
+
+var boolean = true; //true
+console.log(boolean);
+
+var not = !boolean; //false  ! = not
+console.log(not);
+
+var and = boolean && false; //false  && = and
+console.log(and);
+
+var or = boolean || false; //true  || = or
+console.log(or);
+
+var equals = true == false; //false  == = equals
+console.log(equals);

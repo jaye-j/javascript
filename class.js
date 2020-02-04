5a5ad89a707238021fdc512bf2bd34f3fc803d68
jaye-j <jacobrobertjensen@gmail.com>
2020-02-04 11:29:23 -0600

    js practice

--- class.js
+++ class.js
@@ -1,50 +1,64 @@
 // 02/04/2020 class work //
-console.log('hello');
+// console.log('hello');
 
-document.body.innerHTML = 'Hello World';
+// document.body.innerHTML = 'Hello World';
 
-//
-var school = 'dig crafts'; //string
+// //
+// var school = 'dig crafts'; //string
 
-console.log(school);
+// console.log(school);
 
-function myFunction(){
+// function myFunction(){
 
+// };
+
+// //
+// var multiLineString = `This
+// is a
+// multi line
+// string`;
+
+// console.log(multiLineString);
+// console.log(multiLineString.length);
+
+// var myInt = 2.1; //float
+
+// var myBool = true; //boolean
+
+// var num = 1; //int
+
+// var num2 = 1.2; //float
+
+// console.log(school + ' ' + multiLineString); //concatenation
+
+// var concat = "hello" + ' ' + "world";
+// console.log(concat.indexOf("world"));
+
+// var boolean = true; //true
+// console.log(boolean);
+
+// var not = !boolean; //false  ! = not
+// console.log(not);
+
+// var and = boolean && false; //false  && = and
+// console.log(and);
+
+// var or = boolean || false; //true  || = or
+// console.log(or);
+
+// var equals = true == false; //false  == = equals
+// console.log(equals);
+
+var a = 4;
+var b = 4;
+
+if(a == b){
+    console.log("equality")
 };
 
-//
-var multiLineString = `This
-is a
-multi line
-string`;
 
-console.log(multiLineString);
-console.log(multiLineString.length);
+var age = 21;
 
-var myInt = 2.1; //float
-
-var myBool = true; //boolean
-
-var num = 1; //int
-
-var num2 = 1.2; //float
-
-console.log(school + ' ' + multiLineString); //concatenation
-
-var concat = "hello" + ' ' + "world";
-console.log(concat.indexOf("world"));
-
-var boolean = true; //true
-console.log(boolean);
-
-var not = !boolean; //false  ! = not
-console.log(not);
-
-var and = boolean && false; //false  && = and
-console.log(and);
-
-var or = boolean || false; //true  || = or
-console.log(or);
-
-var equals = true == false; //false  == = equals
-console.log(equals);
+if(age >= 21){
+    console.log('you get free beer.')
+};

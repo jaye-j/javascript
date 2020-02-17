0708170228eceb442d5074bce1203563723b933a
jaye-j <jacobrobertjensen@gmail.com>
2020-02-17 10:32:50 -0600

    classwork

---
+++ ajaxnotes.js
@@ -0,0 +1,28 @@
+/////AJAX
+//AJAX = Asychronous JavaScript and XML
+//AJAX allows us to change the page without reloading it. We can retrieve data from somewhere and then use it to change stuff on the page as the visitor is looking at it.
+
+//The XMLHttpRequest object is the key to AJAX. This object is often referred to as the XHR object for short.
+
+
+
+/////FETCH in browser for JS
+
+// fetch(url);
+// .then((response)=>{
+// let json = response.json()
+// });
+// .then((json)=>{
+// we do magic
+// });
+
+
+
+
+// console.log('comment 1');
+// setTimeout(() =>{
+//     console.log('comment 2');
+// }, 1000);
+// console.log('comment 3');
+
+

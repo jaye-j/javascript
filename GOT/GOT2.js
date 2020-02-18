ecf6e919c84765d6b1dba316cc58312899fa3d3f
jaye-j <jacobrobertjensen@gmail.com>
2020-02-18 15:23:16 -0600

    GOT done

--- GOT/GOT2.js
+++ GOT/GOT2.js
@@ -18,7 +18,7 @@ for(let i = 0; i <= 64; i++){
         infoArr = [...infoArr, ...characters];
     })
     .then(()=>{
-    console.log(infoArr);
+    // console.log(infoArr);
         if (i == 64){
             for(let i = 0; i < infoArr.length; i++){
                 if(infoArr[i].allegiances){
@@ -28,22 +28,44 @@ for(let i = 0; i <= 64; i++){
                         return response.json()
                         
                     .then((houseArr) =>{
-
+                        // console.log(houseArr)
                         let house = houseArr.name
-                        console.log(house)
+                        // console.log(house)
+
+                        let button = document.createElement('button')
+                        button.setAttribute('dataHouse', houseArr.url)
+
+                        button.innerHTML = house
+                        button.addEventListener('click', (event)=>{
+                            console.log(event.target.getAttribute('dataHouse'))
+
+
+                            fetch(event.target.getAttribute('dataHouse'))
+                            .then((response)=>{
+                                return response.json()
+                            })
+                            .then((obj)=>{
+
+                                console.log(obj);
+                                window.alert("Name: " + obj.name + " – " + "Sworn members: " + obj.swornMembers.length)
+                            })
+                        })
 
                         if (!infoArr[i].name && !infoArr[i].aliases){
                             let listchar = document.createElement('li');
-                            listchar.innerHTML = `Unknown – <button>${house}</button>`
+                            listchar.innerHTML = `Unknown – `
+                            listchar.append(button)
                             charlist.append(listchar);
                         }
                         else if (!infoArr[i].name){
                             let listchar = document.createElement('li');
-                            listchar.innerHTML = `${infoArr[i].aliases} – <button>${house}</button>`;
+                            listchar.innerHTML = `${infoArr[i].aliases} – `
+                            listchar.append(button);
                             charlist.append(listchar);
                         }else{
                             let listchar = document.createElement('li');
-                            listchar.innerHTML = `${infoArr[i].name} – <button>${house}</button>`;
+                            listchar.innerHTML = `${infoArr[i].name} – `;
+                            listchar.append(button)
                             charlist.append(listchar);
                         }
                     })
@@ -69,3 +91,15 @@ for(let i = 0; i <= 64; i++){
 }
 
 
+// document.querySelector('button').addEventListener('onclick',(event)=>{
+//     fetch(event.target.dataHouseUrl)
+// })
+
+// let elementArr = document.querySelectorAll('button')
+                        
+// elementArr.forEach((button) => {
+//     button.addEventListener('click',(event)=>{
+//         console.log(event.target)
+//         // fetch(event.target.dataHouseUrl)
+//     })
+// })
